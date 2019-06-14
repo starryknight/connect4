11fa1961dc67f738ce22f42e2c058b5a110601a1
kordaro mitchell <kody_mitchell@ymail.com>
2019-06-14 09:58:10 -0400

    declare variables

---
+++ js/project.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    console.log("Hello from app.js")
+    
+    /*declare variables
+   *rows, columns and spots on the board
+   */
+  var column = 7;
+  var row = 6;
+  var boardCircles = [];
+  var players = [];
+
+});
